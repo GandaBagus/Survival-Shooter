5e0032b7ec702d537d8ba585345aa50bf2831018
GandaBagus <81636945+GandaBagus@users.noreply.github.com>
2021-10-21 20:35:48 +0700

    Update EnemyHealth.cs

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -23,32 +23,42 @@ public class EnemyHealth : MonoBehaviour
     private void Awake()
     {
         powerUpManager = FindObjectOfType<PowerUpManager>();
+        //Menapatkan reference komponen
         _anim = GetComponent<Animator>();
         _enemyAudio = GetComponent<AudioSource>();
         _hitParticles = GetComponentInChildren<ParticleSystem>();
         _capsuleCollider = GetComponent<CapsuleCollider>();
 
+        //Set current health
         currentHealth = startingHealth;
     }
 
 
     private void Update()
     {
+        //Check jika sinking
         if (_isSinking) transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
     }
 
 
     public void TakeDamage(int amount, Vector3 hitPoint)
     {
+        //Check jika dead
         if (_isDead) return;
 
+        //play audio
         _enemyAudio.Play();
 
+        //kurangi health
         currentHealth -= amount;
 
+         //Ganti posisi particle
         _hitParticles.transform.position = hitPoint;
+        
+         //Play particle system
         _hitParticles.Play();
 
+        //Dead jika health <= 0
         if (currentHealth <= 0) Death();
     }
 
@@ -56,12 +66,17 @@ public class EnemyHealth : MonoBehaviour
     private void Death()
     {
         powerUpManager.SpawnOnDeath(gameObject.transform);
+        
+        //set isdead
         _isDead = true;
 
+         //SetCapcollider ke trigger
         _capsuleCollider.isTrigger = true;
 
+        //trigger play animation Dead
         _anim.SetTrigger(Dead);
 
+        //Play Sound Dead
         _enemyAudio.clip = deathClip;
         _enemyAudio.Play();
     }
@@ -69,7 +84,10 @@ public class EnemyHealth : MonoBehaviour
 
     public void StartSinking()
     {
+        //disable Navmesh Component
         GetComponent<NavMeshAgent>().enabled = false;
+        
+        //Set rigisbody ke kimematic
         GetComponent<Rigidbody>().isKinematic = true;
         _isSinking = true;
         ScoreManager.score += scoreValue;
