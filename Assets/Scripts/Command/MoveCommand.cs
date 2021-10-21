3b3b7bb3d873778e076d7953afbc97349fcb56c5
GandaBagus <81636945+GandaBagus@users.noreply.github.com>
2021-10-21 20:35:53 +0700

    Update MoveCommand.cs

--- Assets/Scripts/Command/MoveCommand.cs
+++ Assets/Scripts/Command/MoveCommand.cs
@@ -11,16 +11,21 @@ namespace Command
             _h = h;
             _v = v;
         }
-
+        //Trigger perintah movement
         public override void Execute()
         {
             _playerMovement.Move(_h, _v);
+            //Menganimasikan player
+
             _playerMovement.Animating(_h, _v);
         }
 
         public override void UnExecute()
         {
+            //Invers arah dari movement player
             _playerMovement.Move(-_h, -_v);
+            
+            //Menganimasikan player
             _playerMovement.Animating(_h, _v);
         }
     }
